dfb6508834836ccdf61606a1cae53e4afe5f3cb7
shuvo881 <golammostofa10001@gmail.com>
2025-09-25 10:59:45 +0600

    added middilware

--- virtual_try_on/settings.py
+++ virtual_try_on/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pqj(bx)=!s4a43)%k-+ct^fqgbf9(#a*)%#r-b%mqy!(e27h5(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,9 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'channels',
+    'face_detection',
+    'models_manager',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
